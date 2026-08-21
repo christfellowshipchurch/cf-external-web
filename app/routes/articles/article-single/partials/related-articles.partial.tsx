3e7cc0d309788b609c79c192395daac17c523b60
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2026-08-21 11:15:02 -0400

    Move related articles from Algolia to Rock

--- app/routes/articles/article-single/partials/related-articles.partial.tsx
+++ app/routes/articles/article-single/partials/related-articles.partial.tsx
@@ -1,96 +1,28 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { Configure, InstantSearch, useHits } from 'react-instantsearch';
-import { format } from 'date-fns';
 
 import { CardCarouselSection } from '~/components/resource-carousel';
 import { RelatedArticleCard } from '../components/related-article-card.components';
 import { CollectionItem } from '~/routes/page-builder/types';
 import { AuthorProps } from './hero.partial';
-import { createSearchClient } from '~/lib/create-search-client';
-import { ContentItemHit } from '~/routes/search/types';
 import { LoaderReturnType } from '../loader';
 
 export function RelatedArticles() {
-  const data = useLoaderData<LoaderReturnType>();
-  const {
-    ALGOLIA_APP_ID,
-    ALGOLIA_SEARCH_API_KEY,
-    id,
-    articlePrimaryCategories,
-    algoliaIndexes,
-  } = data;
-
-  const searchClient = useMemo(
-    () =>
-      createSearchClient(ALGOLIA_APP_ID || '', ALGOLIA_SEARCH_API_KEY || ''),
-    [ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY],
-  );
-
-  return (
-    <InstantSearch
-      indexName={algoliaIndexes.contentItems}
-      searchClient={searchClient}
-    >
-      <Configure
-        filters={`contentType:"Article" AND articlePrimaryCategories:"${articlePrimaryCategories[0]}" AND rockItemId != ${id}`}
-        hitsPerPage={6}
-      />
-      <CardCarouselSectionWrapper
-        articlePrimaryCategories={articlePrimaryCategories}
-      />
-    </InstantSearch>
-  );
-}
-
-const CardCarouselSectionWrapper = ({
-  articlePrimaryCategories,
-}: {
-  articlePrimaryCategories: string[];
-}) => {
-  const { items } = useHits<ContentItemHit>();
-
-  if (items.length === 0) {
-    return null;
-  }
+  const { articlePrimaryCategories, relatedArticles } =
+    useLoaderData<LoaderReturnType>();
+  if (!relatedArticles?.articles.length) return null;
 
   return (
     <CardCarouselSection
       title='Related Reading'
       description='Explore more articles that you might find interesting.'
-      resources={items.map((item) => ({
-        id: String(item.rockItemId ?? item.objectID),
-        contentChannelId: '',
-        contentType: 'ARTICLES',
-        name: item.title,
-        summary: item.summary,
-        image: item.coverImage.sources[0]?.uri ?? '',
-        pathname: item.url,
-        startDate: format(new Date(item.startDateTime), 'd MMM yyyy'),
-        readTime: item.articleReadTime,
-        authorProps: {
-          fullName:
-            item.author.firstName && item.author.lastName
-              ? `${item.author.firstName} ${item.author.lastName}`
-              : 'Christ Fellowship Team',
-          photo: {
-            uri:
-              item.author.profileImage ||
-              'http://cloudfront.christfellowship.church/GetImage.ashx?guid=A62B2B1C-FDFF-44B6-A26E-F1E213285153',
-          },
-          authorAttributes: {
-            pathname: `${item.author.firstName}-${item.author.lastName}`
-              .toLowerCase()
-              .replace(/ /g, '-'),
-          },
-        },
-      }))}
+      resources={relatedArticles.articles}
       viewMoreText='More Articles'
       viewMoreLink={`/articles?articlePrimaryCategories=${articlePrimaryCategories[0]}`}
       CardComponent={RelatedArticleCardWrapper}
     />
   );
-};
+}
 
 // Wrapper component to adapt RelatedArticleCard to ResourceCarousel's interface
 const RelatedArticleCardWrapper: React.FC<{
